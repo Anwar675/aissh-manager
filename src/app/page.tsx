2bd68fb612c5cb48fad3dd547d73216af3377ec2
almedanwar <doanthienan54@gmail.com>
2026-05-24 19:02:38 +0700

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-"use client";
-import JobPanel from "@/components/dashboard/ui/job-paine";
-import { LineHeader } from "@/components/dashboard/ui/line-header";
-import { SectionCards } from "@/components/dashboard/ui/section-card";
-import { SiteHeader } from "@/components/dashboard/ui/side-headr";
-import { AppSidebar } from "@/components/ui/app-sidebar";
+import { redirect } from "next/navigation";
 
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
-
-const Page = () => {
- 
-  return (
-    <div>
-      Hello work
-    </div>
-  )
-};
-
-export default Page;
+export default function HomePage() {
+  redirect("/dashboard");
+}
